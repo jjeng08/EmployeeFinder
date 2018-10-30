1703f7cefa847344dddf202107e4789ddf58f7c5
jjeng08 <jjeng08@users.noreply.github.com>
2018-10-30 10:01:50 -0400

    Started a CSS file and designed the home page.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function (app) {
 	app.get('/survey', function(req, res) {
-		res.sendFile(path.join(__dirname, '/../public/happy.html'));
+		res.sendFile(path.join(__dirname, '/../public/survey.html'));
 	});
 
 	app.get('*', function(req, res) {
