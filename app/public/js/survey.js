d0cf2dd35fcd19ef22c0cd05a6bd02b1ab307eea
jjeng08 <jjeng08@users.noreply.github.com>
2018-11-02 13:38:40 -0400

    Got all the information displaying properly after lots of debugging.

--- app/public/js/survey.js
+++ app/public/js/survey.js
@@ -9,7 +9,15 @@ $(function () {
 			employeePic: $('#picInput').val().trim(),
 			scores: [
 				parseInt($("input[name='q1']:checked").val()),
-				parseInt($("input[name='q2']:checked").val())
+				parseInt($("input[name='q2']:checked").val()),
+				parseInt($("input[name='q3']:checked").val()),
+				parseInt($("input[name='q4']:checked").val()),
+				parseInt($("input[name='q5']:checked").val()),
+				parseInt($("input[name='q6']:checked").val()),
+				parseInt($("input[name='q7']:checked").val()),
+				parseInt($("input[name='q8']:checked").val()),
+				parseInt($("input[name='q9']:checked").val()),
+				parseInt($("input[name='q10']:checked").val())
 			]
 		}
 
@@ -24,6 +32,7 @@ $(function () {
 			$('#errorBox').toggleClass("alt");
 		} else {
 			errorBox.style.display = "none";
+
 			$.ajax({
 				method: 'POST',
 				url: '/api/employees',
@@ -31,13 +40,14 @@ $(function () {
 			}).then(function (data) {
 				const partnerName = $('#partnerName');
 				const partnerPic = $('#partnerPic');
+				const partnerBio = $('#partnerBio');
 
 				partnerName.empty();
 				partnerBio.empty();
 				partnerPic.empty();
 
 				partnerName.append(`<div id="actualName">${data.employeeName} </div>`);
-				partnerBio.append(`"${data.employeeBio}"`)
+				partnerBio.append(`<div>"${data.employeeBio}"</div>`)
 				partnerPic.append(`<img id="actualPic" src ="${data.employeePic}">`)
 			})
 
@@ -57,10 +67,6 @@ $(function () {
 					modal.style.display = "none";
 				}
 			}
-
-
 		}
-
-
 	}
 })
